dc99c134ebb5a486562b64b4a3902d2eb07a4cf6
tudorgalatan <tudorgalatan@gmail.com>
2019-12-03 12:09:01 +0200

    Small improvements on the general design.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const { src, dest, watch } = require('gulp');
 const scss = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
+const concat = require('gulp-concat');
 
 
 
 function css ()
 {
-  return src('./src/scss/*.scss')
-    .pipe(scss())
-    .pipe(minifyCSS())
-    .pipe(dest('./src/css'))
+	return src('./src/scss/*.scss')
+	.pipe(scss())
+	.pipe(minifyCSS())
+	.pipe(dest('./src/css'))
 }
 
 
@@ -20,5 +21,26 @@ function watchCode ()
 }
 
 
+
+function getJS ()
+{
+	return src('node_modules/bootstrap/dist/js/bootstrap.min.js', { sourcemaps: true })
+	.pipe(concat('app.min.js'))
+	.pipe(dest('src/js', { sourcemaps: true }))
+}
+
+
+
+function getCSS ()
+{
+	return src('node_modules/bootstrap/dist/css/bootstrap.min.css', { sourcemaps: true })
+	.pipe(concat('app.min.css'))
+	.pipe(dest('src/css', { sourcemaps: true }))
+}
+
+
+
 exports.css = css;
 exports.watch = watchCode;
+exports.getJS = getJS;
+exports.getCSS = getCSS;
